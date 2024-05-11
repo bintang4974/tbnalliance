30c9bca9bbc5e53883b24f397af1df9e5609d56f
bintang4974 <bintangramadhan4974@gmail.com>
2024-05-11 17:39:17 +0700

    fixing module ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -80,7 +80,11 @@ class TicketController extends Controller
      */
     public function edit($id)
     {
-        //
+        $title = "Edit Ticket";
+        $ticket = Ticket::find($id);
+        $program = Program::all();
+
+        return view('ticket.edit', compact('title', 'ticket', 'program'));
     }
 
     /**
@@ -92,7 +96,25 @@ class TicketController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'no_ticket' => 'required',
+            'place' => 'required',
+            'date' => 'required',
+            'type' => 'required',
+            'program_id' => 'required',
+        ]);
+
+        $ticket = Ticket::find($id);
+
+        $ticket->update([
+            'no_ticket' => $request->no_ticket,
+            'place' => $request->place,
+            'date' => $request->date,
+            'type' => $request->type,
+            'program_id' => $request->program_id,
+        ]);
+
+        return redirect()->route('ticket.index')->with('success', 'Speaker berhasil diperbarui.');
     }
 
     /**
@@ -103,6 +125,8 @@ class TicketController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Ticket::find($id)->delete();
+
+        return redirect()->route('program.index')->with('success', 'program berhasil dihapus.');
     }
 }
--- resources/views/ticket/edit.blade.php
+++ resources/views/ticket/edit.blade.php
@@ -6,51 +6,61 @@
     <div class="col-lg-6">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <a href="{{ route('program.index') }}" class="btn btn-danger btn-sm"><i class="fas fa-arrow-left"></i>
+                <a href="{{ route('ticket.index') }}" class="btn btn-danger btn-sm"><i class="fas fa-arrow-left"></i>
                     Back</a>
             </div>
             <div class="card-body">
-                <form action="{{ route('program.update', $program->id) }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('ticket.update', $ticket->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" name="name" class="form-control" value="{{ $program->name }}">
-                        @error('name')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Desc</label>
-                        <textarea name="desc" class="form-control">{{ $program->desc }}</textarea>
-                        @error('desc')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label>Time</label>
-                        <input type="text" name="time" class="form-control" value="{{ $program->time }}">
-                        @error('time')
+                        <label>No Ticket</label>
+                        <input type="text" name="no_ticket" class="form-control" value="{{ $ticket->no_ticket }}">
+                        @error('no_ticket')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="form-group">
                         <label>Place</label>
-                        <input type="text" name="place" class="form-control" value="{{ $program->place }}">
+                        <input type="text" name="place" class="form-control" value="{{ $ticket->place }}">
                         @error('place')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label>Speaker</label>
-                        <input type="text" name="speaker" class="form-control" value="{{ $program->speaker }}">
-                        @error('speaker')
+                        <label>Date</label>
+                        <input type="date" name="date" class="form-control" value="{{ $ticket->date }}">
+                        @error('date')
                             <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label>Poster</label>
-                        <input type="file" name="poster" class="form-control">
+                        <label>Type</label>
+                        <select name="type" class="form-control">
+                            <option value="{{ $ticket->type }}">{{$ticket->type}}</option>
+                            <option value="Berbayar">Berbayar</option>
+                            <option value="Gratis">Gratis</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label">Program</label>
+                        <select name="program_id" class="form-control">
+                            @php
+                                $selected = '';
+                                if ($errors->any()) {
+                                    $selected = old('program');
+                                } else {
+                                    $selected = $ticket->program_id;
+                                }
+                            @endphp
+                            @foreach ($program as $program)
+                                <option value="{{ $program->id }}" {{ $selected == $program->id ? 'selected' : '' }}>
+                                    {{ $program->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('program_id')
+                            <div class="text-danger"><small>{{ $message }}</small></div>
+                        @enderror
                     </div>
 
                     <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Save</button>
